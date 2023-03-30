8f9728c638b39d1d0c8a68e66468b4a917a4abdc
mikky_123 <115384802+Nitinkiet@users.noreply.github.com>
2023-03-30 18:25:17 +0530

    Time: 416 ms (51.36%), Space: 144.7 MB (49.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<=2;cap++){
-                    if(buy){
-                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+    int f(int ind,int trans,int n,vector<int>&price,vector<vector<int>>&dp){
+        if(ind==n || trans>3)return 0;
+        
+        if(dp[ind][trans]!=-1)return dp[ind][trans];
+        if(trans%2==0){
+                        return dp[ind][trans] = max(-price[ind] + f(ind+1,trans+1,n,price,dp) ,f(ind+1,trans,n,price,dp));
+    }
+       else {
+          return dp[ind][trans] = max(price[ind] +f(ind+1,trans+1,n,price,dp), f(ind+1,trans,n,price,dp));
 }
-                    else{
-                          dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], dp[i+1][0][cap]);
-}
-}
-}
-}
-        return dp[0][1][2];
+        
+        
+    }
+    int maxProfit(vector<int>& price) {
+        int n = price.size();
+        vector<vector<int>>dp(n+1,vector<int>(5,-1));
+
+//             for(int i=0;i<=3;i++){
+//                 dp[n][i]=0;
+//             }
+
+//             for(int i=n-1;i>=0;i--){
+//                 for(int trans = 0;trans<=3;trans++){
+//                     if(trans%2==0){
+//                         dp[i][trans] = max(-price[i] + dp[i+1][trans+1] , dp[i + 1 ][trans]);
+//     }
+//                     else {
+//                          dp[i][trans] = max(price[i] + dp[i+1][trans+1] , dp[i + 1 ][trans]);
+//     }
+//     }
+//     }
+//             return dp[0][4];
+        
+        return f(0,0,n,price,dp);
+        
     }
 };
