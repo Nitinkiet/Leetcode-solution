9fc889c391f1a31a3d5df3fe43465158c7ae8dda
mikky_123 <115384802+Nitinkiet@users.noreply.github.com>
2023-04-07 04:05:08 +0530

    Time: 8 ms (52.89%), Space: 13.9 MB (9.37%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,7 +6,10 @@ public:
         int ans=INT_MIN,mx=1,mn=1;
         for(int i=0;i<n;i++){
             if(nums[i]<0)swap(mx,mn);
-
+            if(nums[i]==0){
+                mn=1;
+                mx=1;
+}
             mx = max(mx*nums[i],nums[i]);
             mn = min(mn*nums[i],nums[i]);
             ans = max(ans,mx);
