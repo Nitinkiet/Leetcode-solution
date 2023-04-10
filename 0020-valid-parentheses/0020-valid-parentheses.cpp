8566746bd0e04daa575464a71876a2db041f2ccf
mikky_123 <115384802+Nitinkiet@users.noreply.github.com>
2023-04-10 14:23:29 +0530

    Time: 2 ms (46.33%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())return false;
+                if((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']') || (st.top()=='{' && s[i]=='}'))st.pop();
+                else return false;;
+            }
+        }
+        // cout<<st.size()<<" ";
+        if(st.empty())return true;
+        else return false;
+    }
+};
